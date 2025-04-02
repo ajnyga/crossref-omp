875554f57be882a687f07f49e146e76aac75ed5a
felipecorral <felipecorral@gmail.com>
2025-04-02 18:21:20 +0200

    Intermediate comit, manage collection for monographs

--- filter/CrossrefXmlFilter.php
+++ filter/CrossrefXmlFilter.php
@@ -30,6 +30,7 @@ use PKP\core\PKPString;
 use PKP\filter\FilterGroup;
 use PKP\i18n\LocaleConversion;
 use PKP\plugins\importexport\native\filter\NativeExportFilter;
+use APP\file\SubmissionFile;
 
 class CrossrefXmlFilter extends NativeExportFilter
 {
@@ -280,10 +281,76 @@ class CrossrefXmlFilter extends NativeExportFilter
 		$doiDataNode = $doc->createElementNS($deployment->getNamespace(), "doi_data");
 		$doiDataNode->appendChild($node = $doc->createElementNS($deployment->getNamespace(), 'doi', $this->xmlEscape($doi)));
 		$doiDataNode->appendChild($node = $doc->createElementNS($deployment->getNamespace(), 'resource', $this->xmlEscape($url)));
+        // Publication formats
+        $validFormats = array_filter($publicationFormats, function($format) {
+            return $format->getIsAvailable() && 
+            $format->getIsApproved();
+        });
+        error_log(print_r($validFormats, true));
+        $this->appendTextMiningCollectionNodes($doc, $doiDataNode, $submission, $validFormats);
 		$bookMetadataNode->appendChild($doiDataNode);
 
 		return $bookMetadataNode;
 	}
+
+    /**
+     * Append the collection node 'collection property="text-mining"' to the doi data node.
+     *
+     * @param \DOMDocument $doc
+     * @param \DOMElement $doiDataNode
+     * @param \APP\submission\Submission $submission
+     * @param array $publicationFormats Array of \PKP\publicationFormats\PublicationFormat objects
+     */
+    public function appendTextMiningCollectionNodes($doc, $doiDataNode, $submission, $publicationFormats)
+    {
+        $deployment = $this->getDeployment();
+        $context = $deployment->getContext();
+        $request = Application::get()->getRequest();
+        $dispatcher = $this->_getDispatcher($request);
+    
+        $textMiningCollectionNode = $doc->createElementNS($deployment->getNamespace(), 'collection');
+        $textMiningCollectionNode->setAttribute('property', 'text-mining');
+    
+        // Obtener todos los archivos de prueba asociados a la monografía
+        $submissionFiles = Services::get('submissionFile')->getMany([
+            'submissionIds' => [$submission->getId()],
+            'fileStages' => [SubmissionFile::SUBMISSION_FILE_PROOF],
+        ]);
+    
+        foreach ($publicationFormats as $publicationFormat) {
+            $formatId = $publicationFormat->getId();
+    
+            // Filtrar archivos relacionados con este formato
+            $formatFiles = array_filter($submissionFiles, function($file) use ($formatId) {
+                return $file->getAssocType() === ASSOC_TYPE_PUBLICATION_FORMAT &&
+                       $file->getAssocId() === $formatId;
+            });
+    
+            foreach ($formatFiles as $file) {
+                // Construir URL personalizada: /catalog/view/{submission_id}/{pub_format_id}/{file_id}
+                $resourceUrl = $dispatcher->url(
+                    $request,
+                    PKPApplication::ROUTE_PAGE,
+                    $context->getPath(),
+                    'catalog',
+                    'view',
+                    [$submission->getId(), $formatId, $file->getFileId()],
+                    null,
+                    null,
+                    true
+                );
+    
+                $itemNode = $doc->createElementNS($deployment->getNamespace(), 'item');
+                $resourceNode = $doc->createElementNS($deployment->getNamespace(), 'resource', $resourceUrl);
+                $resourceNode->setAttribute('mime_type', $file->getData('mimetype'));
+    
+                $itemNode->appendChild($resourceNode);
+                $textMiningCollectionNode->appendChild($itemNode);
+            }
+        }
+    
+        $doiDataNode->appendChild($textMiningCollectionNode);
+    }    
 
     /**
      * Create and return the Crossref book series metadata node 'series_metadata'.
